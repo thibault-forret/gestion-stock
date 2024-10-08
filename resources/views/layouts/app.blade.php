070a824d14c53afbfa6b1cd9e8d8e313942a5473
thibault-forret <forret.thibault@gmail.com>
2024-10-08 16:55:55 +0200

    add dashboard and redirection

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr" translate="no">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title') - {{ config('app.name') }}</title>
+    <meta name="description" content="@yield('description')">
+    <link 
+        rel="icon" 
+        type="image/png" 
+        sizes="32x25"
+        href="{{ asset('images/logo.webp') }}"
+    >
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+
+@include('components._header') 
+
+<body>
+
+    <div class="content">
+
+        @yield('content')
+
+    </div>
+    
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    @hasSection('js')
+    	@yield('js')
+    @endif    
+
+</body>
+
+@include('components._footer')
+
+</html>
---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+@section('description', 'Affiche un aperçu sur l\'entrepôt.')
+
+@section('content')
+    Dashboard
+@endsection
