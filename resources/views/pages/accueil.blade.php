de2af40cd2b2a3da24d192c4f2cd56611c9d0174
thibault-forret <forret.thibault@gmail.com>
2024-11-03 21:12:37 +0100

    add some basic content

---
+++ resources/views/pages/accueil.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+{{-- Faire en FR et EN (lang) --}}
+@section('title', 'Accueil')
+@section('description', 'Permet de sélectionner son moyen de connexion, entrepot ou magasin.')
+
+@section('content')
+    Accueil
+@endsection
