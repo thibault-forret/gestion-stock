cabcf2f2cb9ab892eaec18356f94ccd9b8428e16
thibault-forret <forret.thibault@gmail.com>
2024-11-05 11:27:08 +0100

    add css file

--- resources/views/pages/warehouse/dashboard.blade.php
+++ resources/views/pages/warehouse/dashboard.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+    <link href="{{ mix('css/pages/warehouse/dashboard.css') }}" rel="stylesheet">
+@endsection
+
 @section('title', __('title.dashboard'))
 @section('description', __('description.dashboard.warehouse'))
 
