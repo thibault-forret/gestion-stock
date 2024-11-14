6aaed0603b99e75f3c003372c627394fa399a096
thibault-forret <forret.thibault@gmail.com>
2024-11-14 13:54:50 +0100

    category model done

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category_name',
+        'category_description',
+    ];
 }
