693e7d4e264589540df082230568cd2d30ec2705
thibault-forret <forret.thibault@gmail.com>
2024-11-14 12:07:47 +0100

    users migration

---
+++ database/migrations/2024_11_14_085639_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name', 50)->nullable(false);
+            $table->string('first_name', 50)->nullable(false);
+            $table->string('user_email')->nullable(false);
+            $table->string('user_password')->nullable(false);
+            $table->unsignedBigInteger('role_id')->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
