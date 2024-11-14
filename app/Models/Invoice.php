1f57c6d76e1542340370b665a7a585e9bfaa0889
thibault-forret <forret.thibault@gmail.com>
2024-11-14 21:47:26 +0100

    correct mistake on fk

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -16,11 +16,11 @@ class Invoice extends Model
 
     public function order()
     {
-        return $this->belongsTo(Order::class);
+        return $this->belongsTo(Order::class, 'order_id', 'id');
     }
 
     public function supply()
     {
-        return $this->belongsTo(Supply::class);
+        return $this->belongsTo(Supply::class, 'supply_id', 'id');
     }
 }
