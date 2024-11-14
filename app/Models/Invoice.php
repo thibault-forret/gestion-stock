225598667d28edc4e1cc25ea31df967ffcbf6389
thibault-forret <forret.thibault@gmail.com>
2024-11-14 13:57:25 +0100

    invoice model done

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    protected $fillable = [
+        'invoice_number',
+        'invoice_date',
+        'invoice_status',
+        'order_id',
+        'supply_id'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function supply()
+    {
+        return $this->belongsTo(Supply::class);
+    }
 }
