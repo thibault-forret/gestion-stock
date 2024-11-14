5099d09aaf21cbd1aa3ebc5271e5afa449e28143
thibault-forret <forret.thibault@gmail.com>
2024-11-14 10:19:06 +0100

    product migration

--- database/migrations/2024_11_14_085542_create_product_table.php
+++ database/migrations/2024_11_14_085542_create_product_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('product', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name', 100)->nullable(false);
+            $table->text('product_description')->nullable(false);
+            $table->decimal('reference_price', 10, 2)->nullable(false);
+            $table->int('restock_threshold')->nullable(false);
+            $table->int('alert_treshold')->nullable(false);
+            $table->unsignedBigInteger('category_id')->nullable(false);
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('cascade');
         });
     }
 
