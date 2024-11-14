52e4d1462c30ab47d05fa0e0146a9530e7e764e0
thibault-forret <forret.thibault@gmail.com>
2024-11-14 11:58:05 +0100

    supplies migration

---
+++ database/migrations/2024_11_14_085628_create_supplies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('supplier_id')->nullabe(false);
+            $table->unsignedBigInteger('product_id')->nullabe(false);
+            $table->unsignedBigInteger('warehouse_id')->nullable(false);
+            $table->date('supply_date')->nullable(false);
+            $table->int('quantity_supplied')->nullable(false);
+            $table->decimal('unit_price', 10, 2)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplies');
+    }
+};
