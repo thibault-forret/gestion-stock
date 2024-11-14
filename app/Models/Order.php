fe5929d0b9f04cd7222799af065316718492d435
thibault-forret <forret.thibault@gmail.com>
2024-11-14 21:47:38 +0100

    order model done

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'warehouse_id',
+        'order_date',
+        'order_status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class, 'warehouse_id', 'id');
+    }
 }
