51a27c8f6d3d5509a615102672a4a0f268d6d30a
thibault-forret <forret.thibault@gmail.com>
2024-11-14 10:29:46 +0100

    stock_movement migration

--- database/migrations/2024_11_14_085614_create_stock_movement_table.php
+++ database/migrations/2024_11_14_085614_create_stock_movement_table.php
@@ -11,9 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stock_movement', function (Blueprint $table) {
+        Schema::create('stock_movements', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id')->nullable(false);
+            $table->unsignedNBigInteger('warehouse_id')->nullable(false);
+            $table->unsignedBigInteger('user_id')->nullable(false);
+            $table->int('quantity_moved')->nullable(false);
+            // replace entry and exit by IN and OUT
+            $table->enum('movement_type', ['IN', 'OUT'])->nullable(false);
+            $table->date('movement_date')->nullable(false);
+            $table->enum('movement_status', ['IN_PROGRESS', 'COMPLETED', 'CANCELLED'])->nullable(false);
+            $table->string('movement_source')->nullable(false);
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->foreign('warehouse_id')->references('id')->on('warehouse');
+            $table->foreign('user_id')->references('id')->on('user');
         });
     }
 
