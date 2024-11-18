2fa7b265965ba14811c6d4967707f95badf4fd1c
thibault-forret <forret.thibault@gmail.com>
2024-11-18 23:14:55 +0100

    changement mix config et ajout de commentaire explicatif

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,39 +1,29 @@
 const mix = require('laravel-mix');
-const fs = require('fs');
-const path = require('path');
 
-// Fonction pour récupérer tous les fichiers sauf ceux qui commencent par un underscore
-function getFiles(dir, fileType) 
-{
-    let files = fs.readdirSync(dir);
-    let fileList = [];
-
-    files.forEach(file => {
-        let filePath = path.join(dir, file);
-        let stat = fs.statSync(filePath);
-
-        if (stat.isDirectory()) {
-            fileList = fileList.concat(getFiles(filePath, fileType));
-        } else if (filePath.endsWith(fileType) && !path.basename(file).startsWith('_')) {
-            fileList.push(filePath);
-        }
-    });
-
-    return fileList;
-}
-
-// Récupère tous les fichiers SCSS sauf ceux qui commencent par un underscore
-const scssFiles = getFiles('resources/scss', '.scss');
-
-// Compile chaque fichier SCSS trouvé
-scssFiles.forEach(file => {
-    let output = file.replace('resources/scss', 'public/css').replace('.scss', '.css');
-    mix.sass(file, output).version();;
-});
-
-// Compile les fichiers JavaScript
+// Compile les fichiers JavaScript et SCSS en fichiers CSS et JS respectifs.
 mix.js('resources/js/app.js', 'public/js')
+
+    .sass('resources/scss/style.scss', 'public/css/')
+
+    // Pages
+    .sass('resources/scss/pages/login.scss', 'public/css/pages/')
+    .sass('resources/scss/pages/home.scss', 'public/css/pages/')
+
+    // Warehouse
+    .sass('resources/scss/pages/warehouse/dashboard.scss', 'public/css/pages/warehouse/')
+
+    // Store
+    .sass('resources/scss/pages/store/dashboard.scss', 'public/css/pages/store/')
+
+
+    // Fonctionnement :
+    // JS : mix.js('chemin_du_fichier_js', 'chemin_du_fichier_js') -> Voir exemple ci-dessus
+    // SCSS : mix.sass('chemin_du_fichier_scss', 'chemin_du_fichier_css') -> Voir exemple ci-dessus
+
+    // Versionnage des fichiers
     .version();
 
 // Versionnage ".version()" des fichiers afin de générer des url uniques pour chaques fichier compilé. Cela permet de forcer les navigateurs
 // à récupérer les nouvelles verisons des fichiers après une mise à jour, plutôt que d'utiliser une version mise en cache.
+
+// Pour compiler les fichiers, il suffit de lancer la commande "npm run watch" pour compiler les fichiers en temps réel.
