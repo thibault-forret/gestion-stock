18f8662efda3e4b8b207552f50b8e1d262d5534c
thibault-forret <forret.thibault@gmail.com>
2024-12-03 10:55:34 +0100

    make submit function

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -99,7 +99,43 @@ class StockController extends Controller
 
     public function supplyProductSubmit(Request $request) 
     {
+        $request->validate([
+            'stock_id' => 'required|integer|exists:stocks,id',
+            'quantity' => 'required|integer|min:1',
+        ],
+        [
+            'stock_id.required' => __('messages.validate.stock_id_required'),
+            'stock_id.integer' => __('messages.validate.stock_id_integer'),
+            'stock_id.exists' => __('messages.stock_not_found'),
+            'quantity.required' => __('messages.validate.quantity_required'),
+            'quantity.integer' => __('messages.validate.quantity_integer'),
+            'quantity.min' => __('messages.validate.quantity_min'),
+        ]);
 
+        // Vérifier si la quantité est inférieure à la capacité maximale
+        $quantity = $request->input('quantity');
+
+        $user = auth()->user();
+
+        $warehouse = $user->warehouseUser->warehouse;
+
+        // Vérifier si la quantité dépasse la capacité de l'entrepôt
+        if (($quantity + $warehouse->stock->sum('quantity_available')) > $warehouse->capacity) {
+            return redirect()->back()->withErrors(__('messages.validate.quantity_exceeds_capacity'))->withInput();
+        }
+
+        // Récupérer le stock
+        $stock = Stock::find($request->stock_id);
+
+        // Mettre à jour la quantité disponible
+        $success = $stock->addStock($quantity);
+
+        if ($success) {
+            return redirect()->route('warehouse.stock.index')->with('success', __('messages.action_success'));
+        }
+        else {
+            return redirect()->route('warehouse.stock.index')->with('error', __('messages.action_failed'));
+        }
     }
 
     public function removeProduct() 
