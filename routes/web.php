c41a4aaa989eee331e0054728adc1bc64df575de
thibault-forret <forret.thibault@gmail.com>
2024-12-09 23:41:54 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StockController;
+use App\Http\Controllers\InvoiceController;
 
 
 // Redirige vers dashboard en cas d'erreur sur l'url
@@ -85,6 +86,13 @@ Route::middleware(['web', 'lang.toggle'])->group(function () {
                     Route::post('/remove/quantity', [StockController::class, 'removeQuantityProductSubmit'])->name('remove.quantity.submit');
                     Route::post('/remove/product', [StockController::class, 'removeProductSubmit'])->name('remove.product.submit');
                 });
+            });
+
+            Route::prefix('invoice')->name('invoice.')->group(function () {
+                Route::get('/', [InvoiceController::class, 'invoiceList'])->name('list');
+                Route::get('/filter', [InvoiceController::class, 'filterInvoice'])->name('filter');
+                Route::get('/{invoice_id}/info', [InvoiceController::class, 'infoInvoice'])->name('info');
+                Route::get('/{invoice_id}/settle', [InvoiceController::class, 'settleInvoice'])->name('settle');
             });
         });
     });
