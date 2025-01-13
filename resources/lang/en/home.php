a20ff74d25af987965a87f8760a3bf7567c32f65
thibault-forret <forret.thibault@gmail.com>
2025-01-13 20:12:52 +0100

    add welcome messages

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'welcome_title' => 'Welcome to Nova Solutions',
+    'welcome_message' => 'Your ideal partner for efficient stock management',
     'warehouse_service_title' => 'Warehouse Management',
     'warehouse_service_description' => 'Monitor your inventory, track incoming and outgoing items, and optimize your warehouse space.',
     'store_service_title' => 'Store Management',
