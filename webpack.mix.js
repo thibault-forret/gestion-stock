2307509cd6d549638266b2c44a9b8788d3ac219b
Tom Fleter <104789517+yeezhy@users.noreply.github.com>
2025-01-13 22:35:32 +0100

    add frontend product_add

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ mix.js('resources/js/app.js', 'public/js')
     // Warehouse
     .sass('resources/scss/pages/warehouse/dashboard.scss', 'public/css/pages/warehouse/')
     .sass('resources/scss/pages/warehouse/search_new_product.scss', 'public/css/pages/warehouse/')
+    .sass('resources/scss/pages/warehouse/add_product.scss', 'public/css/pages/warehouse/')
 
     // Store
     .sass('resources/scss/pages/store/dashboard.scss', 'public/css/pages/store/')
