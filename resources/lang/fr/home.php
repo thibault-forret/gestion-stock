5ec044de94c760b63c843d4b76b91d82014231f5
thibault-forret <forret.thibault@gmail.com>
2025-01-14 12:38:18 +0100

    modify nova solutions to nova stock

--- resources/lang/fr/home.php
+++ resources/lang/fr/home.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'welcome_title' => 'Bienvenue sur Nova Solutions',
+    'welcome_title' => 'Bienvenue sur Nova Stock',
     'welcome_message' => 'Votre partenaire idéal pour une gestion de stock efficace',
     'warehouse_service_title' => 'Gestion des entrepôts',
     'warehouse_service_description' => 'Supervisez vos stocks, suivez les entrées et sorties, et optimisez l’espace de vos entrepôts.',
