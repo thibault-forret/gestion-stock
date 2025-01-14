8472320a1d407bdfa10dc082fce82ff96e853999
thibault-forret <forret.thibault@gmail.com>
2025-01-14 10:28:01 +0100

    remove double notif

--- resources/views/pages/warehouse/product/search_new_product.blade.php
+++ resources/views/pages/warehouse/product/search_new_product.blade.php
@@ -11,18 +11,6 @@
 
 @section('content')
 
-    @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-
-    @if (session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
-
     <div class="filter-buttons">
         <form action="{{ route('warehouse.product.search') }}" method="GET">
             <div class="filter-group">
