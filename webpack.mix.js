5c36a962b9ee6c34f1eaf27ed0c18ebd0fc8f61e
Tom Fleter <104789517+yeezhy@users.noreply.github.com>
2025-01-14 11:11:04 +0100

    add frontend edit_product

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,6 +23,7 @@ mix.js('resources/js/app.js', 'public/js')
 
     .sass('resources/scss/pages/warehouse/stock/movement_list.scss', 'public/css/pages/warehouse/stock/')
     .sass('resources/scss/pages/warehouse/stock/info_product.scss', 'public/css/pages/warehouse/stock/')
+    .sass('resources/scss/pages/warehouse/stock/edit_product.scss', 'public/css/pages/warehouse/stock/')
 
     // Store
     .sass('resources/scss/pages/store/dashboard.scss', 'public/css/pages/store/')
