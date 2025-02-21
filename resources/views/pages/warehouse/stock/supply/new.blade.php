3501121da70fba1efc80b33815c94289a8ff953b
Tom Fleter <104789517+yeezhy@users.noreply.github.com>
2025-02-21 19:25:33 +0100

    warehouse supply new front updated

--- resources/views/pages/warehouse/stock/supply/new.blade.php
+++ resources/views/pages/warehouse/stock/supply/new.blade.php
@@ -10,7 +10,6 @@
 @section('title-content', mb_strtoupper(__('title.new_supply')))
 
 @section('content')
-    
     @if($suppliers->isEmpty())
         <p class="no-suppliers">Aucun produits ajoutés à l'entrepôt, veuillez ajouter des produits</p>
         <a href="{{ route('warehouse.product.index')}}">Ajouter des produits</a>
@@ -18,15 +17,19 @@
 
     <div class="role-selection">
         @foreach($suppliers as $supplier)
-            <form action="{{ route('warehouse.stock.supply.place.new') }}" method="post">
-                @csrf
-                <input type="hidden" name="supplier_id" value="{{ $supplier->id }}">
-                <button class="role-card" type="submit">
-                    <div class="role-title">{{ $supplier->supplier_name }}</div>
-                    <p class="role-description">{{ __('description.new_supply') }}</p>
-                </button>
-            </form>
+            <div class="role-card">
+                <form action="{{ route('warehouse.stock.supply.place.new') }}" method="post">
+                    @csrf
+                    <input type="hidden" name="supplier_id" value="{{ $supplier->id }}">
+                    <button type="submit">
+                        <div class="icon">
+                            <i class="fas fa-shipping-fast"></i>
+                        </div>
+                        <div class="role-title">{{ $supplier->supplier_name }}</div>
+                        <p class="role-description">{{ __('description.new_supply') }}</p>
+                    </button>
+                </form>
+            </div>
         @endforeach
     </div>
-    
 @endsection
